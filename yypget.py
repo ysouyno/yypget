d11cbb33b5a22c732b531776c8644b2715aea338
ysouyno <ysouyno@163.com>
2018-12-22 15:56:00 +0800

    add tqdm progress bar

--- yypget.py
+++ yypget.py
@@ -11,19 +11,15 @@ def r1(pattern, text):
 def download_wrapper(url, output_dir = '.'):
     host = r1(r'https?://([^/]+)/', url)
     assert(host)
-    print('host: %s' % host)
 
     if host.endswith('.com.cn'):
         host = host[:-3]
-        print('host: %s' % host)
 
     domain = r1(r'(\.[^.]+\.[^.]+)$', host)
     assert(domain)
-    print('domain: %s' % domain)
 
     site = r1(r'([^.]+)', domain)
     assert(site)
-    print('site: %s' % site)
 
     supported_sites = {
         'baidu': baidu,
@@ -47,11 +43,9 @@ def main(download):
     args = parser.parse_args()
     if args.url:
         url = args.url
-        print('url: %s' % url)
 
     if args.output_dir:
         output_dir = args.output_dir
-        print('output_dir: %s' % output_dir)
 
     download(url, output_dir)
 
--- yypget/executor/baidu.py
+++ yypget/executor/baidu.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
+import os
 import requests
 from bs4 import BeautifulSoup
-import os
+from tqdm import tqdm
 
 # test url:
 # https://sv.baidu.com/videoui/page/videoland?pd=bjh&context={%22nid%22:%224949783038993399443%22,%22sourceFrom%22:%22bjh%22}&fr=bjhauthor&type=video
@@ -18,21 +19,26 @@ def baidu_download(url, output_dir = '.'):
     # download video
     video_real_url = find_result.video.source.attrs['src']
     assert(video_real_url)
-    print("real url: %s" % video_real_url)
+    print('real url: %s' % video_real_url)
 
     video_title = soup.title.string
     assert(video_title)
-    print('title: %s' % video_title)
 
     video_file = os.path.join(output_dir, video_title)
-    print('video_file: %s' % video_file)
 
     video = requests.get(video_real_url, stream = True)
+    assert(video.status_code == 200)
+
+    t = tqdm(total = int(video.headers['Content-Length']))
+
     with open(video_file + '.mp4', 'wb') as mp4:
-        for chunk in video.iter_content(chunk_size = 1024 * 1024):
+        for chunk in video.iter_content(chunk_size = 1024):
             if chunk:
                 mp4.write(chunk)
+                t.update(len(chunk))
 
-    print('download done')
+    t.close()
+
+    print('download \"%s.mp4\" success' % video_title)
 
 download = baidu_download
