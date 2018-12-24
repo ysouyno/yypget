d5f13c8230eef24dceb5f177635bc1f181a8b3ed
ysouyno <ysouyno@163.com>
2018-12-24 14:28:26 +0800

    get file type from the web

--- yypget/executor/baidu.py
+++ yypget/executor/baidu.py
@@ -1,43 +1,48 @@
 #!/usr/bin/env python3
 
-import os
 import requests
+import os
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+from ..utils import *
 
 # test url:
 # https://sv.baidu.com/videoui/page/videoland?pd=bjh&context={%22nid%22:%224949783038993399443%22,%22sourceFrom%22:%22bjh%22}&fr=bjhauthor&type=video
 
 def baidu_download(url, output_dir = '.'):
     r = requests.get(url)
-    assert(r.status_code == 200)
+    assert r.status_code == 200
 
     soup = BeautifulSoup(r.text, features = 'html.parser')
     find_result = soup.find(id = 'main-player')
-    assert(find_result)
+    assert find_result
 
-    # download video
+    # download
     video_real_url = find_result.video.source.attrs['src']
-    assert(video_real_url)
-
-    video_title = soup.title.string
-    assert(video_title)
-
-    video_file = os.path.join(output_dir, video_title)
+    assert video_real_url
 
     video = requests.get(video_real_url, stream = True)
-    assert(video.status_code == 200)
+    assert video.status_code == 200
+
+    video_file = soup.title.string
+    assert video_file
+
+    ext = get_url_file_ext(video_real_url)
+    assert ext
+
+    video_file = video_file + '.' + ext
+    video_file = os.path.join(output_dir, video_file)
 
     t = tqdm(total = int(video.headers['Content-Length']), ascii = True)
 
-    with open(video_file + '.mp4', 'wb') as mp4:
+    with open(video_file, 'wb') as file:
         for chunk in video.iter_content(chunk_size = 1024):
             if chunk:
-                mp4.write(chunk)
+                file.write(chunk)
                 t.update(len(chunk))
 
     t.close()
 
-    print('download \"%s.mp4\" success' % video_title)
+    print('download \"%s\" success' % video_file)
 
 download = baidu_download
--- yypget/utils.py
+++ yypget/utils.py
@@ -1,6 +1,24 @@
 import re
+import requests
 
 def r1(pattern, text):
     m = re.search(pattern, text)
     if m:
         return m.group(1)
+
+def get_url_file_ext(url):
+    resp = requests.get(url)
+    assert resp.status_code == 200
+
+    type = resp.headers['Content-Type']
+    type_dict = {
+        'video/3gpp': '3gp',
+        'video/f4v': 'flv',
+        'video/mp4': 'mp4',
+        'video/webm': 'webm',
+        'video/x-flv': 'flv'
+    }
+
+    assert type in type_dict, type
+
+    return type_dict[type]
