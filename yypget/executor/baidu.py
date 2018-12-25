d2fe9e5a1faa3dca4d5495bcdf40b9365562870f
ysouyno <ysouyno@163.com>
2018-12-25 13:35:47 +0800

    \r make output in one line

--- yypget/executor/baidu.py
+++ yypget/executor/baidu.py
@@ -2,13 +2,15 @@
 
 from urllib import request
 import os
+import sys
 from ..utils import *
 
 # test url:
 # https://sv.baidu.com/videoui/page/videoland?pd=bjh&context={%22nid%22:%224949783038993399443%22,%22sourceFrom%22:%22bjh%22}&fr=bjhauthor&type=video
 
 def urlretrieve_callback(count, size, total):
-    print('count: %s, size: %s, total: %s' % (count, size, total))
+    sys.stdout.write('\rcount: %s, size: %s, total: %s' % (count, size, total))
+    sys.stdout.flush()
 
 def baidu_download(url, output_dir = '.'):
     resp = request.urlopen(url)
@@ -34,6 +36,6 @@ def baidu_download(url, output_dir = '.'):
     video_file = os.path.join(output_dir, video_file)
 
     request.urlretrieve(true_url, video_file, urlretrieve_callback)
-    print('download success: %s' % video_file)
+    print('\ndownload success: %s' % video_file)
 
 download = baidu_download
