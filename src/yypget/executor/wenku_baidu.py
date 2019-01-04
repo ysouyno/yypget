67c59d3d95d7ab34f54d18429e81ea8b20363adb
ysouyno <ysouyno@163.com>
2019-01-04 16:38:22 +0800

    can get the content of txt, but the content order is chaotic

--- src/yypget/executor/wenku_baidu.py
+++ src/yypget/executor/wenku_baidu.py
@@ -3,13 +3,14 @@
 from urllib import request
 import json
 import os
+import gzip
 from ..utils import *
 
 url_list = []
 
 # test url: https://wenku.baidu.com/view/6f4ca758312b3169a451a4a7.html
 
-def get_txt(page_url, path_file):
+def get_doc(page_url, path_file):
     # print('page_url: %s' % page_url)
     resp = request.urlopen(page_url)
     assert resp, resp.getcode() == 200
@@ -29,17 +30,17 @@ def get_txt(page_url, path_file):
 
     for i in page_json:
         if i['t'] == 'word':
-            txt = i['c']
+            doc = i['c']
 
             if i['ps'] != None and '_enter' in i['ps'].keys():
-                txt = '\n'
+                doc = '\n'
 
-            # print(txt, end = '')
+            # print(doc, end = '')
 
             with open(path_file, 'a') as f:
-                f.write(txt)
+                f.write(doc)
 
-def get_txt_wrapper(urls, path_file):
+def get_doc_wrapper(urls, path_file):
     urls = urls.replace('\\x22', '')
     assert urls
 
@@ -57,7 +58,64 @@ def get_txt_wrapper(urls, path_file):
     # print("url_list length: %s" % len(url_list))
 
     for page_url in url_list:
-        get_txt(page_url, path_file)
+        get_doc(page_url, path_file)
+
+def get_txt(url, path_file):
+    print('get_txt url: %s' % url)
+    resp = request.urlopen(url)
+    assert resp, resp.getcode() == 200
+
+    data = resp.read()
+    assert data
+
+    # UnicodeDecodeError: 'utf-8' codec can't decode byte 0x8b in position 1:
+    # invalid start byte
+    data = gzip.decompress(data).decode('utf-8')
+    assert data
+    # print(data)
+
+    txt_json = json.loads(data.split('(', 1)[1].strip(')'))
+    assert txt_json
+    # print(txt_json)
+
+    for page in txt_json:
+        print(page['parags'][0]['c'])
+
+def get_txt_wrapper(url, path_file):
+    print('get_txt_wrapper url: %s' % url)
+    resp = request.urlopen(url)
+    assert resp, resp.getcode() == 200
+
+    data = resp.read()
+    assert data
+
+    data = data.decode('utf-8')
+    assert data
+
+    json_data = json.loads(data.split('(', 1)[1].strip(')'))
+    assert json_data
+    # print('json_data: %s' % json_data)
+
+    doc_id = json_data['doc_id']
+    print('doc_id: %s' % doc_id)
+
+    md5sum = json_data['md5sum']
+    print('md5sum: %s' % md5sum)
+
+    json_doc_info = json_data['docInfo']
+    assert json_doc_info
+
+    total_page_num = json_doc_info['totalPageNum']
+    print('totalPageNum: %s' % total_page_num)
+
+    txt_json_url = 'https://wkretype.bdimg.com/retype/text/'
+    txt_json_url += doc_id
+    txt_json_url += '?'
+    txt_json_url += md5sum
+    txt_json_url += '&callback=cb&pn=1&rn='
+    txt_json_url += total_page_num
+
+    get_txt(txt_json_url, path_file)
 
 def wenku_baidu_download(url, output_dir = '.'):
     resp = request.urlopen(url)
@@ -82,17 +140,23 @@ def wenku_baidu_download(url, output_dir = '.'):
     assert doc_type
     print('doc_type: %s' % doc_type)
 
-    wkinfo_htmlurls = r1(r'WkInfo.htmlUrls\s+\=\s+\'(.+)\'\;', data)
-    assert wkinfo_htmlurls
-    # print(wkinfo_htmlurls)
-
     output_dir = os.path.realpath(output_dir)
     path_file = doc_title + '.txt'
     path_file = os.path.join(output_dir, path_file)
     # print(path_file)
 
     if doc_type == 'doc':
-        get_txt_wrapper(wkinfo_htmlurls, path_file)
+        wkinfo_htmlurls = r1(r'WkInfo.htmlUrls\s+\=\s+\'(.+)\'\;', data)
+        assert wkinfo_htmlurls
+        # print(wkinfo_htmlurls)
+
+        get_doc_wrapper(wkinfo_htmlurls, path_file)
+    elif doc_type == 'txt':
+        getdocinfo_url = r'https://wenku.baidu.com/api/doc/getdocinfo?callback=cb&doc_id='
+        getdocinfo_url += doc_id
+        get_txt_wrapper(getdocinfo_url, path_file)
+    else:
+        print('Neither doc nor txt')
 
     if os.path.exists(path_file) and os.path.getsize(path_file) > 0:
         print('Download OK: %s' % path_file)
